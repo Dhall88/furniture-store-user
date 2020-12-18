9be3d3eb23e824fb878fc7aee4581b36ee46497e
David Hall <davidchall388@gmail.com>
2020-12-18 11:45:51 -0500

    working iwth passport and jsonwebtokens

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const passport = require('./config/passport');
 const User = require("./models/User")
 const router = express.Router();
 
